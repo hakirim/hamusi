ab8752d548235b912eb9ba6b26b97a0f958269b4
sutonea <stonhair@gmail.com>
2022-09-10 13:20:58 +0900

    Menu for control servers. close #9

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,19 @@ use cli_clipboard::{ClipboardContext, ClipboardProvider};
 use donoha::client::APIClient;
 use donoha::client::APIToken;
 use donoha::client::APITokenRequest;
-//use donoha::types::Servers;
-//use std::time;
-//
-//const DONOHA_KEY_USER_NAME: &str = "DONOHA_USER_NAME";
-//const DONOHA_KEY_TENANT_ID: &str = "DONOHA_TENANT_ID";
-//const DONOHA_KEY_PASSWORD: &str = "DONOHA_PASSWORD";
+use donoha::types::Server;
+
+const DONOHA_KEY_TENANT_ID: &str = "DONOHA_TENANT_ID";
 const DONOHA_KEY_API_TOKEN: &str = "DONOHA_API_TOKEN";
 fn main() {
-    // let opt_token: Option<APIToken> = std::env::var(DONOHA_KEY_API_TOKEN)
-    //     .ok()
-    //     .and_then(|value| Some(APIToken { value }));
     let opt_token = token_setup_from_env();
-    println!("Input tenant id (NOT tenant name):");
-    let tenant_id = gets();
+    let tenant_id = match std::env::var(DONOHA_KEY_TENANT_ID) {
+        Ok(tenant_id) => tenant_id,
+        Err(_) => {
+            println!("Input tenant id (NOT tenant name):");
+            gets()
+        }
+    };
     let token = match opt_token {
         Some(token) => token,
         None => loop {
@@ -33,30 +32,6 @@ fn main() {
     };
     let client = APIClient::new(&token);
     menu_initial(&client, &tenant_id);
-    //    let api_client = APIClient::new(&api_token);
-    //    let result = api_client.servers_text(tenant_id.clone()).unwrap();
-    //    let servers: Servers = serde_json::from_str(&result).unwrap();
-    //    for server in servers.servers {
-    //        print!("shutdown...");
-    //        if api_client.shutdown(&server) {
-    //            println!("OK.");
-    //        } else {
-    //            println!("NG.");
-    //        }
-    //        print!("delete...");
-    //        if api_client.delete(&server) {
-    //            println!("OK.");
-    //        } else {
-    //            println!("NG.");
-    //            println!("Retrying...");
-    //            std::thread::sleep(time::Duration::from_secs(10));
-    //            if api_client.delete(&server) {
-    //                println!("OK.");
-    //            } else {
-    //                println!("NG.");
-    //            }
-    //        }
-    //    }
     println!("DONE");
 }
 
@@ -77,7 +52,7 @@ fn menu_initial(client: &APIClient, tenant_id: &String) {
             std::process::exit(0);
         }
         if str_in == "1" {
-            menu_listing_server(&client, tenant_id.clone())
+            menu_listing_server(&client, tenant_id)
         }
 
         ()
@@ -110,19 +85,87 @@ fn generate_token(user_name: String, tenant_id: &String) -> Option<APIToken> {
         .ok()
 }
 
-fn menu_listing_server(client: &APIClient, tenant_id: String) {
+fn menu_listing_server(client: &APIClient, tenant_id: &String) {
     // サーバー取得
-    let servers = client.servers(tenant_id);
-    match servers {
-        Some(servers) => {
-            println!("Number of servers: {}", servers.servers.len());
-            for (i, server) in servers.servers.iter().enumerate() {
-                if 0 == i {
-                    println!("id, name");
-                }
-                println!("{},{}", server.id, server.name);
+    loop {
+        let servers = client.servers((*tenant_id).clone()).unwrap();
+
+        println!("");
+        println!("Number of servers: {}", servers.servers.len());
+        for (i, server) in servers.servers.iter().enumerate() {
+            let no = i + 1;
+            if 0 == i {
+                println!("No, status, id, name");
             }
+            println!("{}. {} {} {}", no, server.status, server.id, server.name);
         }
-        None => {}
+        println!("r. Reload list.");
+        println!("0. Quit");
+
+        println!("Input [No] of target server.");
+
+        let str = gets();
+        if str == "r" {
+            continue;
+        }
+        if str == "0" {
+            std::process::exit(0);
+        }
+        let no: usize = match str.parse() {
+            Ok(n) => n,
+            Err(_) => {
+                continue;
+            }
+        };
+        let server: &Server = match servers.servers.get(no - 1) {
+            Some(s) => s,
+            None => {
+                continue;
+            }
+        };
+        menu_control_server(client, &server);
+    }
+}
+
+fn menu_control_server(client: &APIClient, server: &Server) {
+    loop {
+        // メニュー表示
+        println!("b. Boot a server");
+        println!("s. Stop a server");
+        println!("d. Delete a server");
+        println!("0. Back");
+
+        // メニュー番号を入力させる
+        let str = gets();
+        let mut list_str = str.split_whitespace();
+        let operation: &str = match list_str.nth(0) {
+            Some(s) => s,
+            None => {
+                println!("invalid operation");
+                continue;
+            }
+        };
+
+        if operation == "b" {
+            print!("Send boot request...");
+            client.boot(server);
+            println!("done.");
+            return;
+        } else if operation == "s" {
+            print!("Send shutdown request...");
+            client.shutdown(server);
+            println!("done.");
+            return;
+        } else if operation == "d" {
+            print!("Send delete request...");
+            client.delete(server);
+            println!("done.");
+            return;
+        } else if operation == "0" {
+            println!("Back");
+            return;
+        } else {
+            continue;
+        }
     }
 }
